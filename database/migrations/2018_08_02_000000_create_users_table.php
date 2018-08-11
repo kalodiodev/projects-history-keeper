b1cbe76db357679aa202208311b12d44304e99cd
Athanasios Raptodimos <thanosra@gmail.com>
2018-08-11 16:08:00 +0300

    Fix database migrations ordering

--- database/migrations/2018_08_02_000000_create_users_table.php
+++ database/migrations/2018_08_02_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('role_id')->unsigned();
+            $table->integer('role_id')->unsigned();
             $table->rememberToken();
             $table->timestamps();
 
