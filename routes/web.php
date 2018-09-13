b64b8c7bc17e7df23bf7a0f4c422908a014d25e6
Athanasios Raptodimos <thanosra@gmail.com>
2018-09-13 20:35:42 +0300

    Remove welcome page

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 // Authentication Routes...
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login');
@@ -30,7 +26,11 @@ Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail'
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
-Route::get('/home', 'HomeController@index')->name('home');
+
+/*
+ * Home / Dashboard
+ */
+Route::get('/', 'HomeController@index')->name('home');
 
 /*
  * User Routes
